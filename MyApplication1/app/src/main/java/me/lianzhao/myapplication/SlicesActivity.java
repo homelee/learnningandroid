e03bcdee864600b8e38251e4fffb90f7b319b3d6
lianzhao <knife.fe@gmail.com>
2015-04-09 19:13:52 +0800

    test http request

--- MyApplication1/app/src/main/java/me/lianzhao/myapplication/SlicesActivity.java
+++ MyApplication1/app/src/main/java/me/lianzhao/myapplication/SlicesActivity.java
@@ -1,39 +1,71 @@
 package me.lianzhao.myapplication;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 
 public class SlicesActivity extends ActionBarActivity {
 
+    @InjectView(R.id.ivSlice)
+    ImageView ivSlice;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
         setContentView(R.layout.activity_slices);
+        ButterKnife.inject(this);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_slices, menu);
-        return true;
+    public void getImage(View view) {
+        String requestUri = "";// todo
+        new DownloadImageTask().execute(requestUri);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    private class DownloadImageTask extends AsyncTask<String, Void, String> {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        @Override
+        protected String doInBackground(String... params) {
+            String uri = params[0];
+            try {
+                // todo
+                return "https://www.baidu.com/img/bdlogo.png";
+            } catch (Exception ex) {
+                return ex.getMessage();
+            }
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        protected void onPostExecute(String result) {
+            try {
+                URL url = new URL(result);
+                URLConnection urlConnection = url.openConnection();
+                InputStream inputStream = urlConnection.getInputStream();
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                ivSlice.setImageBitmap(bitmap);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
